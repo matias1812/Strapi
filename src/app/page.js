937264c4621219b11a7eb07dec1d9fa06ad15a81
matias torres <matias.torres1812@gmail.com>
2023-08-08 07:44:15 -0400

    SNARKDOWN

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { getGames, getCoverImages } from "../services/games";
-
+import { mdToHTML } from "./snarkdown";
 export default async function Home() {
   const games = await getGames();
   console.log(games);
@@ -25,9 +25,7 @@ export default async function Home() {
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               {attributes.title}
             </h5>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              {attributes.descripcion}
-            </p>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400" dangerouslySetInnerHTML={{__html: mdToHTML(attributes.descripcion) }} />
           </div>
         </Link>
       ))}
--- src/services/games.js
+++ src/services/games.js
@@ -2,7 +2,7 @@ import { API_URL, STRAPI_URL } from "../app/config";
 
 export async function getGames() {
     const res = await fetch(
-      `${API_URL}/video-games?populate[platforms][fields][0]=name&populate[cover][fields][0]=url`
+      `${API_URL}/video-games?populate[platforms][fields][1]=name&populate[cover][fields][1]=url`
     )
     if (!res.ok) {
       throw new Error("problemas tecnicos");
